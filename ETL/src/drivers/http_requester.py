97e7544874a5eb23663304d312d07191d1c77471
Bruna <beebruna@outlook.com>
2025-03-11 15:17:45 -0300

    feat: Implementing HttpRequester

---
+++ ETL/src/drivers/http_requester.py
@@ -0,0 +1,15 @@
+from typing import Dict, Union
+import requests
+
+class HttpRequester:
+
+    def __init__(self) -> None:
+        self.__url = "https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm"
+
+    def request_from_page(self) -> Dict[str, Union[int, str]]:
+        response = requests.get(self.__url)
+        return {
+            "status_code": response.status_code,
+            "html": response.text
+        }
+    
